0ec76b931704adaa70f15ddcbf7331e9ea605741
unknown <Delimond-eng>
2023-11-16 00:51:30 +0200

    database relationship

--- database/migrations/2023_11_15_145015_create_agents_table.php
+++ database/migrations/2023_11_15_145015_create_agents_table.php
@@ -15,16 +15,20 @@ return new class extends Migration
     {
         Schema::create('agents', function (Blueprint $table) {
             $table->integer('agent_id', true);
-            $table->string('agent_matricule', 25)->unique();
-            $table->string('agent_nom', 50);
-            $table->string('agent_prenom', 50);
-            $table->char('agent_sexe', 1);
-            $table->string('agent_telephone', 15);
+            $table->string('agent_matricule')->unique();
+            $table->string('agent_nom');
+            $table->string('agent_prenom');
+            $table->char('agent_sexe');
+            $table->string('agent_telephone');
             $table->integer('agent_service_id');
             $table->integer('agent_grade_id');
             $table->integer('agent_fontion_id');
             $table->timestamp('agent_create_At')->useCurrentOnUpdate()->useCurrent();
-            $table->string('agent_status', 10)->default('actif');
+            $table->string('agent_status')->default('actif');
+
+            $table->foreign('agent_service_id')->references('service_id')->on('services')->onDelete('cascade');
+            $table->foreign('agent_grade_id')->references('grade_id')->on('grades')->onDelete('cascade');
+            $table->foreign('agent_fontion_id')->references('fonction_id')->on('fonctions')->onDelete('cascade');
         });
     }
 
--- database/migrations/2023_11_15_145015_create_consultation_details_table.php
+++ database/migrations/2023_11_15_145015_create_consultation_details_table.php
@@ -15,12 +15,14 @@ return new class extends Migration
     {
         Schema::create('consultation_details', function (Blueprint $table) {
             $table->integer('consult_detail_id', true);
-            $table->string('consult_detail_libelle', 50);
-            $table->string('consult_detail_valeur', 50);
+            $table->string('consult_detail_libelle');
+            $table->string('consult_detail_valeur');
             $table->text('consult_detail_obs');
             $table->timestamp('consult_detail_create_At')->useCurrent();
             $table->string('consult_detail_status', 10)->default('actif');
             $table->integer('consult_id');
+
+            $table->foreign('consult_id')->references('consut_id')->on('consultations')->onDelete('cascade');
         });
     }
 
