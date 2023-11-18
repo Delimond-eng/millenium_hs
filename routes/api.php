496bbdf991d8746fc0fb29dc978f6297d7d3fe21
unknown <Delimond-eng>
2023-11-19 00:24:03 +0200

    cors middleware

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/agents",[ AgentController::class, 'all']);
 
+Route::middleware(['cors'])->group(function () {
+    Route::post('/hogehoge', 'Controller@hogehoge');
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
