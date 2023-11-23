e7e5cebb63040ae0c1ceb13f38069655ec54538b
unknown <Delimond-eng>
2023-11-23 23:31:56 +0200

    lts db userRole

--- database/migrations/2023_11_23_212342_create_user_roles_table.php
+++ database/migrations/2023_11_23_212342_create_user_roles_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('user_roles', function (Blueprint $table) {
             $table->id();
-            $table->string('role');
+            $table->string('role')->unique();
             $table->timestamps();
         });
     }
